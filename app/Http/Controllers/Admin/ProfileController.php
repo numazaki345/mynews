df1526fe05f917221cf56d10b3a9c279dfe9e40c
numazaki <ryo.091979@gmail.com>
2021-03-29 08:39:08 +0900

    投稿したニュースを更新 / 削除しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -35,13 +35,27 @@ class ProfileController extends Controller
 
         
     }
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $this->validate($request, Profile::$rules);
+        // News Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        return redirect('admin/profile/');
     }
 
     public function index(Request $request)
@@ -55,5 +69,13 @@ class ProfileController extends Controller
           $posts = Profile::all();
       }
       return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
+    public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
   }
 }
